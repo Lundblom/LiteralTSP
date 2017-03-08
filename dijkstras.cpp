cb10559afb622a45d1b8addf7b5996f4ed7f8158
Awardit <lundblom@kth.se>
2017-03-08 19:10:10 +0100

    Comments are nice

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -1,4 +1,3 @@
-#include "FibHeap.h"
 #include <functional>
 #include <queue>
 #include <vector>
@@ -26,39 +25,50 @@ namespace pathfinding
 		std::vector<std::vector<int> > distance(g.size(), std::vector<int>(g.size(), PATH_INFINITY));
 		std::vector<std::vector<Node*> > previous(g.size(), std::vector<Node*>(g.size(), NULL));
 
+		//Tells the queue how to sort the elements
 		auto cmp = [](std::pair<Node*, int> left, std::pair<Node*, int> right) { return left.second > right.second;};
 
+		//Creates a queue containing a Node* and the distance to it (int)
+		//This is wrapped in a vector
+		//The cmp lambda is used to sort the queue
 		std::priority_queue< std::pair<Node*, int>, std::vector <std::pair<Node*, int> > , decltype(cmp) > queue(cmp);
 
+		//Queue init
 		for(int i = 0; i < g.size(); ++i)
 		{
 			for(int j = 0; j < g[i].size(); ++j)
 			{
+				//We dont add the first element yet
 				if(i == start.first && j == start.second)
 				{
 					continue;
 				}
+				//Adds a node with infinity (unknown) distance
 				std::pair<Node*, int> node(g[i][j], PATH_INFINITY);
 				queue.push(node);
 			}
 		}
 
+		//Set distance of start to 0
 		distance[start.first][start.second] = 0;
 		std::pair<Node*, int> startNode(g[start.first][start.second], 0);
 		queue.push(startNode);
 
 		while (!queue.empty())
 		{
+			//Get the node with the lowest distance
 			std::pair<Node*, int> nodePair = queue.top();
 			queue.pop();
 
 			Node* u = nodePair.first;
 
+			//Checks if we are done
 			if(u->position.first == end.first && u->position.second == end.second)
 			{
 				break;
 			}
 
+			//All location offsets from the current node that we check
 			std::vector<std::pair<int, int> >locations = {{0,-1}, {0, 1}, {-1, 0}, {1, 0}};
 
 			for(int i = 0; i < locations.size(); ++i)
@@ -66,6 +76,7 @@ namespace pathfinding
 				int x = locations[i].first + u->position.first;
 				int y = locations[i].second + u->position.second;
 
+				//Bounds check
 				if(x < 0 || y < 0 || x >= g.size() || y >= g.size())
 				{
 					continue;
@@ -76,17 +87,25 @@ namespace pathfinding
 				{
 					continue;
 				}
+
+
 				int a = distance[u->position.first][u->position.second] + v->length;
 
+				//If this path is shorter than the one we are currently on
 				if(a < distance[x][y])
 				{
+					//Sets the new total path distance
 					distance[x][y] = a;
+					//Update our path trace, set the node we came fram
 					previous[x][y] = u;
+					//Push the new node with the new distance set
 					queue.push(std::make_pair(v, distance[x][y]));
 				}
 			}
 		}
 
+		//Creates a stack with our path in it
+		//Stack is used for easy backtracking
 		Node* current = g[end.first][end.second];
 		std::stack<Node*> path;
 		while(current != NULL)
