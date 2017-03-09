52a07fd91b911ae67063e241c468c86065665c8f
Awardit <lundblom@kth.se>
2017-03-09 03:28:07 +0100

    Trying to add data read

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 
 using namespace pathfinding;
 
-#define GRID_SIZE 8
 #define STARTX 0
 #define STARTY 0
 #define ENDX 7
@@ -19,35 +18,13 @@ using namespace pathfinding;
 #define THREADS 20
 #define TIME_BETWEEN_PATHS 100
 
-static bool traversableArr[] = {
-		1,0,0,1,0,0,0,0,
-		0,0,0,0,0,0,1,0,
-		0,1,0,0,0,0,0,0,
-		1,0,0,0,0,0,0,0,
-		0,0,0,0,1,0,0,0,
-		0,0,0,1,0,0,0,0,
-		1,0,1,0,0,0,0,0,
-		0,0,0,0,0,0,0,0,
-		
-
-	};
-
-static int distanceArr[] = {
-		1,1,2,3,1,1,1,3,
-		1,1,1,1,1,1,1,2,
-		1,1,1,1,1,1,3,1,
-		1,1,1,10,10,1,1,1,
-		1,3,1,10,10,1,1,1,
-		1,1,3,1,1,1,3,1,
-		1,1,6,1,1,1,1,3,
-		1,1,1,1,1,4,1,3
-	};
-
 static std::condition_variable printCV;
 static std::mutex printMutex;
 static bool printReady = true;
 
-static std::vector< std::vector< Node* > > graph(GRID_SIZE, std::vector<Node*>(GRID_SIZE, NULL));
+static int GRID_SIZE;
+
+static std::vector< std::vector< Node* > > graph;
 
 void dijkstra_generator(int thread_id)
 {
@@ -63,28 +40,31 @@ void dijkstra_generator(int thread_id)
 			startX = rand() % GRID_SIZE;
 			startY = rand() % GRID_SIZE;
 		}
-		while(traversableArr[startX * GRID_SIZE + startY * GRID_SIZE]);
+		while(!graph[startX][startY]->traversable);
 
 		do
 		{
 			endX = rand() % GRID_SIZE;
 			endY = rand() % GRID_SIZE;
 		}
-		while(traversableArr[endY * GRID_SIZE + endY * GRID_SIZE]);
+		while(!graph[endX][endY]->traversable);
 
 		std::stack<Node*> result = dijkstras(graph, std::make_pair(startX,startY), std::make_pair(endX, endY));
 
+		std::cout << "Out of dijkstras" << std::endl;
+
 		bool pathArr[GRID_SIZE * GRID_SIZE];
 
-		for(int i = 0; i < GRID_SIZE * GRID_SIZE; ++i)
-		{
-			pathArr[i] = 0;
-		}
+		//std::memset(pathArr, 0, sizeof(bool) * GRID_SIZE * GRID_SIZE);
+
+		std::cout << "created pathArr" << std::endl;
 
 		while(!result.empty())
 		{
 			Node* n = result.top();
 			result.pop();
+
+			std::cout << "Got node " << (n->position.first * GRID_SIZE + n->position.second) << std::endl;
 
 			pathArr[n->position.first * GRID_SIZE + n->position.second] = 1;
 		}
@@ -99,17 +79,17 @@ void dijkstra_generator(int thread_id)
 		std::cout << "Start (" << startX << ", " <<  startY << ")" << std::endl;
 		std::cout << "End (" << endX << ", " <<  endY << ")" << std::endl;
 
-		std::cout << "Grid";
+		std::cout << "Grid" << std::endl;
 
-		for(int i = 0; i < GRID_SIZE * GRID_SIZE; ++i)
+		/*
+		for(int i = 0; i < GRID_SIZE; ++i)
 		{
-			if(i % GRID_SIZE == 0)
+			for(int j = 0; j < GRID_SIZE; ++j)
 			{
-				std::cout << std::endl;
+				std::cout << (graph[i][j]->traversable ? "X" : "o") << " ";
 			}
-			std::cout << (traversableArr[i] ? "X" : "o") << " ";
-
-		}
+			std::cout << std::endl;
+		}*/
 		std::cout << std::endl << "Path";
 		for(int i = 0; i < GRID_SIZE * GRID_SIZE; ++i)
 		{
@@ -133,13 +113,33 @@ void dijkstra_generator(int thread_id)
 
 }
 
-int main()
+int main(int argc, char** argv)
 {
-	for(int i = 0; i < GRID_SIZE; ++i)
+
+	int vertices;
+	int gridSize;
+
+	std::cin >> vertices;
+	std::cin >> gridSize;
+
+	std::cout << "vertices: " << vertices << std::endl;
+	std::cout << "gridSize: " << gridSize << std::endl;
+
+	GRID_SIZE = gridSize;
+
+	graph = std::vector< std::vector< Node* > >(GRID_SIZE, std::vector<Node*>(GRID_SIZE, NULL));
+
+	int counter = 0;
+	int rowCounter = 0;
+	while(counter < vertices)
 	{
-		for(int j = 0; j < GRID_SIZE; ++j)
+		bool isTraversable;
+		std::cin >> isTraversable;
+		graph[rowCounter][counter % GRID_SIZE] = new Node(1, std::make_pair(rowCounter, counter % GRID_SIZE), isTraversable);
+		++counter;
+		if(counter % GRID_SIZE == 0)
 		{
-			graph[i][j] = new Node(distanceArr[i * GRID_SIZE + j], std::make_pair(i,j), !traversableArr[i * GRID_SIZE + j]);
+			++rowCounter;
 		}
 	}
 
--- numbers.sh
+++ numbers.sh
@@ -5,13 +5,16 @@ SIDE=1024
 
 COUNTER=0
 
+echo $SIZE
+echo $SIDE
+
 
 while(( COUNTER < SIZE )); do
 	if(( RANDOM % 30 < 28))
 	then
-		printf 0,
+		printf "0 "
 	else
-		printf 1,
+		printf "1 "
 	fi
 
 	if((COUNTER % SIDE == 0))
